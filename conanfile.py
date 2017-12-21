3015e423a12f788b786bb125283b5a3f694a6bc4
Rene Rivera <grafikrobot@gmail.com>
2017-12-20 22:11:19 -0600

    Normalize for 1.66.0 release packaging.

--- conanfile.py
+++ conanfile.py
@@ -1,43 +1,55 @@
-from conans import ConanFile, tools
-import os
+from conans import ConanFile
+
 
 class BoostMp11Conan(ConanFile):
     name = "Boost.Mp11"
     version = "1.66.0"
-    commit_id = "a96ae790a43ad68433c773e8692bdeced24b6929"
-    generators = "boost"
-    settings = "os", "arch", "compiler", "build_type"
-    url = "https://github.com/bincrafters/conan-boost-mp11"
-    description = "Please visit http://www.boost.org/doc/libs/1_66_0/libs/libraries.htm"
-    license = "www.boost.org/users/license.html"
+
+    build_requires = \
+        "Boost.Config/1.66.0@bincrafters/testing" 
+
     lib_short_names = ["mp11"]
-    build_requires = "Boost.Generator/1.65.1@bincrafters/stable" 
-    requires =  "Boost.Config/1.65.1@bincrafters/stable"
+    is_header_only = True
 
-                      #config0
+    # BEGIN
 
-    def source(self):
-        source_url = "https://github.com/boostorg"
-        for lib_short_name in self.lib_short_names:
-            self.run("git clone --branch=master {0}/{1}.git".format(source_url, lib_short_name))
-            with tools.chdir(lib_short_name):
-                self.run("git checkout {0}".format(self.commit_id))
-            
-    # TODO: Switch to this method after 1.66.0 release
-    # def source(self):
-        # boostorg_github = "https://github.com/boostorg"
-        # archive_name = "boost-" + self.version  
-        # for lib_short_name in self.lib_short_names:
-            # tools.get("{0}/{1}/archive/{2}.tar.gz"
-                # .format(boostorg_github, lib_short_name, archive_name))
-            # os.rename(lib_short_name + "-" + archive_name, lib_short_name)
-
-    def package(self):
-        for lib_short_name in self.lib_short_names:
-            include_dir = os.path.join(lib_short_name, "include")
-            self.copy(pattern="*", dst="include", src=include_dir)		
+    url = "https://github.com/bincrafters/conan-boost-mp11"
+    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
+    license = "www.boost.org/users/license.html"
+    build_requires = "Boost.Generator/1.66.0@bincrafters/testing"
+    short_paths = True
+    exports = "boostgenerator.py"
 
     def package_id(self):
         self.info.header_only()
+        getattr(self, "package_id_after", lambda:None)()
+    def source(self):
+        self.call_patch("source")
+    def build(self):
+        self.call_patch("build")
+    def package(self):
+        self.call_patch("package")
+    def package_info(self):
+        self.call_patch("package_info")
+    def call_patch(self, method, *args):
+        if not hasattr(self, '__boost_conan_file__'):
+            try:
+                from conans import tools
+                with tools.pythonpath(self):
+                    import boostgenerator  # pylint: disable=F0401
+                    boostgenerator.BoostConanFile(self)
+            except Exception as e:
+                self.output.error("Failed to import boostgenerator for: "+str(self)+" @ "+method.upper())
+                raise e
+        return getattr(self, method, lambda:None)(*args)
+    @property
+    def env(self):
+        import os.path
+        result = super(self.__class__, self).env
+        result['PYTHONPATH'] = [os.path.dirname(__file__)] + result.get('PYTHONPATH',[])
+        return result
+    @property
+    def build_policy_missing(self):
+        return (getattr(self, 'is_in_cycle_group', False) and not getattr(self, 'is_header_only', True)) or super(self.__class__, self).build_policy_missing
 
-
+    # END
