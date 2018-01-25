ed6c839938153673f88fee6f0473cd0df26f55b3
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:11:55 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostMp11Conan(ConanFile):
     name = "boost_mp11"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_mp11"
-    
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["mp11"]
     is_header_only = True
 
