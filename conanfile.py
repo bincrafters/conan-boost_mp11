ced3b4ccbcf7ac04841b572cf746e9c8c6d89ea6
Rene Rivera <grafikrobot@gmail.com>
2018-12-17 14:02:40 -0600

    Initial 1.69.0 packages. [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,11 +4,10 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.68.0@bincrafters/testing")
+base = python_requires("boost_base/1.69.0@bincrafters/testing")
 
 class BoostMp11Conan(base.BoostBaseConan):
     name = "boost_mp11"
     url = "https://github.com/bincrafters/conan-boost_mp11"
     lib_short_names = ["mp11"]
     header_only_libs = ["mp11"]
-    b2_requires = ["boost_config"]
